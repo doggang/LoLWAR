6612e543e3dfa7e55e50710fe7e394cd8f6f889a
doggang <rlaehgusqp@naver.com>
2025-02-06 00:04:10 +0900

    채팅창쪽 업데이트

--- src/component/Community.jsx
+++ src/component/Community.jsx
@@ -1,7 +1,13 @@
 const Community = ()=>{
   return(
     <community id="community">
-      Community
+      <div id="comTitle">
+        <div id="comTitleImg" />
+        <div id="comTitleStateCover">
+          <div id="comTitleName">말년중위 김도현</div>
+          <div id="comTitleState">●  &nbsp;온라인</div>
+        </div>
+      </div>
     </community>
   );
 }
