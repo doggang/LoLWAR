add00e09bcb6cf50577029d832d45e7fc80269a8
doggang <rlaehgusqp@naver.com>
2025-02-19 00:13:33 +0900

    d

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ViteGitHubPages from 'vite-plugin-gh-pages';
+import ViteGitHubPages from 'vite-plugin-gh-pages'; // 플러그인 import
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteGitHubPages()],
-  assetsInclude: ['**/*.mp3'],
-  base:'/LoLWAR/',
-})
+  plugins: [react()],
+  base: '/LoLWAR/', // 리포지토리 이름에 맞게 수정
+  build: {
+    outDir: 'dist',
+  },
+  // ViteGitHubPages가 자동으로 배포를 처리하도록 설정
+});
